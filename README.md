f099191cff895eb45baa74dc6ec8aec3981cbbaf
Oliver Smith <oliver@kfs.org>
2020-03-15 23:51:20 -0700

    Simple filesystem benchmarking code

---
+++ README.md
@@ -0,0 +1,36 @@
+# Filesystem 'open()' benchmark
+
+Benchmark of the filesystems performing opening files.
+
+I was going to use 'open()' to eliminate the stdio overhead, but it's actually comparatively tiny
+for the case of opening a file, and it made the code slightly more portable (lookin' at you win32).
+
+# Building
+
+cmake -DCMAKE\_BUILD\_TYPE=Release
+cmake --build . --target SimpleBench
+
+and then run the SimpleBench executable.
+
+This will create ~1000 one-line text files in the current directory and then repeatedly open them,
+capturing the timing, and then deleting them.
+
+# Outputs
+
+Output on a high-end 2019 i7 on Samsung nvme drive/Windows:
+```
+Iteration Stats (4096 files): min=56ms, avg=59ms, p90=62ms, p95=63ms, p99=68ms, max=77ms
+Per File: 14411ns
+```
+
+Output from an Ubuntu 18.04 VM on an Intel NUC with a SATA 2 SSD:
+```
+Iteration Stats (4096 files): min=10ms, avg=11ms, p90=12ms, p95=13ms, p99=15ms, max=18ms
+Per File: 2786ns
+```
+
+Output from an Ubuntu 19.04 Linux machine on an EVO SSD:
+```
+Iteration Stats (4096 files): min=2ms, avg=4ms, p90=4ms, p95=5ms, p99=6ms, max=7ms
+Per File: 914ns
+```
---
+++ simplebench.cpp
@@ -0,0 +1,128 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <cstdint>
+#include <numeric>
+#include <string>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif
+
+using std::chrono::steady_clock;
+using std::chrono::nanoseconds;
+using std::chrono::duration_cast;
+
+
+struct FileList {
+	std::vector<std::string> mFiles;
+
+	const std::string& operator[](size_t index) const noexcept {
+		return mFiles[index];
+	}
+
+	bool is_valid() const noexcept {
+		return !mFiles.empty();
+	}
+
+	FileList(size_t count) : mFiles{}
+	{
+		mFiles.resize(count);
+		for (size_t i = 0; i < count; ++i) {
+			mFiles[i] = "testfile." + std::to_string(i) + ".txt";
+			auto fp = fopen(mFiles[i].c_str(), "w");
+			if (fp == nullptr) {
+				fprintf(stderr, "ERROR: Couldn't create %s\n", mFiles[i].c_str());
+				throw std::runtime_error("Unable to create files\n");
+			}
+			fprintf(fp, "-- this file intentionally blank[ish] --\n");
+			fclose(fp);
+		}
+	}
+
+	~FileList() noexcept
+	{
+		for (const auto& filename : mFiles) {
+			unlink(filename.c_str());
+		}
+	}
+};
+
+
+constexpr auto percentile = [](const auto& timings, size_t percentile) -> size_t
+{
+	const size_t length = timings.size();
+	const size_t point = length * percentile;
+	// If we wouldn't produce a whole number, take the average of the values either side
+	if (((point) % 100) != 0) {
+		auto left = timings[size_t(std::floor(double(point) / 100.))];
+		auto right = timings[size_t(std::ceil(double(point) / 100.))];
+		return (left + right) / 2;
+	}
+	return timings[(point / 100)];
+};
+
+
+int main()
+{
+	constexpr size_t NumIters{ 1000 };
+	constexpr size_t NumFiles{ 4096 };
+	constexpr size_t TotalOps{ NumIters * NumFiles };
+
+	FileList fileList{ NumFiles };
+
+	std::vector<uint64_t> iterTimings{};
+	iterTimings.reserve(NumIters);
+
+	constexpr size_t ReportEvery{ 100 };
+	for (size_t iter = 0; iter < NumIters; ++iter) {
+		if ((iter % ReportEvery) == 0) {
+			printf("Iter %zu/%zu\n", iter, NumIters);
+		}
+
+		const auto start = steady_clock::now();
+		for (const auto& filename : fileList.mFiles) {
+			FILE* fp = fopen(filename.c_str(), "r");
+			if (!fp) {
+				fprintf(stderr, "Unable to open %s\n", filename.c_str());
+				exit(1);
+			}
+			fclose(fp);
+		}
+
+		const auto end = steady_clock::now();
+		const uint64_t nanos = duration_cast<nanoseconds>(end - start).count();
+		iterTimings.push_back(nanos);
+	}
+
+	uint64_t total = std::accumulate(iterTimings.cbegin(), iterTimings.cend(), 0ULL);
+	uint64_t avg = total / iterTimings.size();
+
+	// Order them so we can print min, max, p90 and p95
+	std::sort(iterTimings.begin(), iterTimings.end());
+	uint64_t lowest = iterTimings.front();
+	uint64_t p90 = percentile(iterTimings, 90);
+	uint64_t p95 = percentile(iterTimings, 95);
+	uint64_t p99 = percentile(iterTimings, 99);
+	uint64_t highest = iterTimings.back();
+
+	const char* unit = "ns";
+	uint64_t divisor = 1;
+	if (lowest > 10 * 1000 * 1000) {
+		unit = "ms";
+		divisor = 1000 * 1000;
+	}
+	else if (lowest > 10 * 1000) {
+		unit = "us";
+		divisor = 1000;
+	}	
+
+	printf("Iteration Stats (%zu files): min=%zu%s, avg=%zu%s, p90=%zu%s, p95=%zu%s, p99=%zu%s, max=%zu%s\n",
+		NumFiles,
+		lowest / divisor, unit, avg / divisor, unit, p90 / divisor, unit, p95 / divisor, unit, p99 / divisor, unit, highest / divisor, unit);
+	printf("Per File: %zuns\n", (total / TotalOps));
+}
